cc3e1d7d5ed4263ddb6ff314d700856f2394e9de
Adisie Fenta <adisiefenta23@gmail.com>
2024-03-10 13:52:17 +0300

    some changes

--- tailwindcss/src/App.jsx
+++ tailwindcss/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+// pages
+import ReactTable from "./pages/ReactTable";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h3 className="text-2xl my-3 font-black text-green-700">
+        Tables In React
+      </h3>
+      <ReactTable />
+    </div>
+  );
 };
 
 export default App;
---
+++ tailwindcss/src/pages/ReactTable.jsx
@@ -0,0 +1,102 @@
+const ReactTable = () => {
+  return (
+    <div className="flex items-center justify-center overflow-hidden">
+      <div className="max-w-[700px] mx-auto w-full overflow-x-auto">
+        <table className="w-full">
+          <thead className="border-b-2 border-gray-300">
+            <tr className="bg-green-500 text-white">
+              <th className="text-lg text-left font-semibold text-gray-100 py-2 px-2 transition-all ease-in-out duration-300 hover:bg-green-400 cursor-pointer whitespace-nowrap">
+                First Name
+              </th>
+              <th className="text-lg text-left font-semibold text-gray-100 py-2 px-2 transition-all ease-in-out duration-300 hover:bg-green-400 cursor-pointer whitespace-nowrap">
+                Last Name
+              </th>
+              <th className="text-lg text-left font-semibold text-gray-100 py-2 px-2 transition-all ease-in-out duration-300 hover:bg-green-400 cursor-pointer">
+                Email
+              </th>
+              <th className="text-lg text-left font-semibold text-gray-100 py-2 px-2 transition-all ease-in-out duration-300 hover:bg-green-400 cursor-pointer">
+                Address
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="bg-gray-100 border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+            <tr className="border-b border-gray-200">
+              <td className="px-2 py-1">Haddis</td>
+              <td className="px-2 py-1">Fanta</td>
+              <td className="px-2 py-1">haddisfun@gmail.com</td>
+              <td className="px-2 py-1">Addis Ababa,Ethiopia</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ReactTable;
